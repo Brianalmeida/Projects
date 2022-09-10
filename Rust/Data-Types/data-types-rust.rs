90192a2c410c6ed164a046c1de58cfd79e2bb45c
Brian Almeida <73912537+Brianalmeida@users.noreply.github.com>
2022-09-10 18:55:58 +0000

    Create data-types-rust.rs

---
+++ Rust/Data-Types/data-types-rust.rs
@@ -0,0 +1,72 @@
+fn main() {
+
+// Scalar Types:
+
+    // Floating point types
+        // let x = 2.0; // f64
+
+        // let y: f32 = 3.0; // f32
+
+
+
+    // Numeric Operations
+    //
+        // Addition
+    //    let sum = 5 + 10;
+
+        // Subtraction
+    //    let difference = 95.6 - 43.7;
+
+        // Multiplication
+    //    let product = 4 * 40;
+
+        // Division
+    //    let quotient = 56.7 / 32.6;
+    //    let floored = 2 / 3;
+
+        // Remainder
+    //    let remainder = 43 % 5;
+
+    // Boolean type
+    //
+        // let t = true;
+
+        // let f: bool = false;
+
+
+    // Character type
+    //
+        // let c = 'z';
+
+        // let z: char = 'ℤ';
+
+        // let heart_eyed_cat = '😻';
+
+// Compund Types:
+
+    // Tuple type
+    //
+        // let tup = (500, 6.4, 1);
+
+        // let (x, y, z) = tup;
+
+        // println!("The value of y is: {y}");
+
+        // let x: (i32, f64, u8) = (500, 6.4, 1);
+
+        // let five_hundred = x.0;
+
+        // let six_point_four = x.1;
+
+        // let one = x.2;
+
+        let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+        let b = [3; 5];
+
+        let first = a[0];
+        let second = b[2];
+
+        println!("This is first: {first}");
+        println!("This is second: {second}");
+}
