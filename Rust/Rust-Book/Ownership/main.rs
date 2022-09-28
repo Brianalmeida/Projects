324de29c2ea366fc2239cb727dd5db5954caaff2
Brian Almeida <73912537+Brianalmeida@users.noreply.github.com>
2022-09-28 12:54:10 -0400

    Update main.rs

--- Rust/Rust-Book/Ownership/main.rs
+++ Rust/Rust-Book/Ownership/main.rs
@@ -24,23 +24,51 @@ fn main() {
 
     // println!("x = {}, y = {}", x, y);
 
-    let s = String::from("hello");
+    // let s = String::from("hello");
 
-    takes_ownership(s);
+    // takes_ownership(s);
 
-    let x = 5;
+    // let x = 5;
 
-    makes_copy(x);
+//    makes_copy(x);
+
+//    let s1 = gives_ownership();
+//    let s3 = take_and_give_back(s2);
+
+    let s1 = String::from("Hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 
 }
 
-    fn takes_ownership(some_string: String) {
-        println!("{}", some_string);
-    }
+//    fn takes_ownership(some_string: String) {
+//        println!("{}", some_string);
+//    }
 
-    fn makes_copy (some_integer: i32) {
-        println!("{}", some_integer);
-    }
+//    fn makes_copy (some_integer: i32) {
+        // println!("{}", some_integer);
+    // }
 
 
-// Left off at Return Values
+// Return Values
+
+// fn gives_ownership() -> String {
+
+//    let some_string = String::from("yours");
+
+//    some_string
+// }
+
+// fn take_and_give_back(a_string: String) -> String {
+
+//    a_string
+// }
+
+fn calculate_length(s: String) -> (String, usize) {
+
+    let length = s.len();
+
+    (s, length)
+}
